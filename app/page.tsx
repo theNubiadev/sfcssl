0945fb2a17bef0ef3d1c1239bd6cdc433e7f1739
thenubiadev <ajaiyeobajibola@gmail.com>
2026-05-12 23:23:27 +0100

    fix: update background image in Home component to use SVG data URL

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/public/hero.png";
-import { url } from "inspector";
+
 interface Feature {
   label: string;
   icon: React.ElementType;
@@ -83,7 +83,6 @@ export default function Home() {
             className="absolute inset-0 opacity-100"
             style={{
               backgroundImage:
-              // "url('/hero.png')",
                 "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23c8973a' fill-opacity='0.04'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
             }}
           />
